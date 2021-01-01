c874d1b42a781e034980f125db331780d77b5fc2
pdeepak326 <pdeepak326@gmail.com>
2021-01-02 01:28:06 +0530

    fixed active class javascript code

--- javascript.js
+++ javascript.js
@@ -3,9 +3,13 @@
     function highlight() {
         var a = window.location.href.split('/')
         var id = a[a.length - 1].split('.')
-        var name = '#' + id[0]
-        console.log(name)
-        $(name).addClass('active')
+        var name = '#' + id[0];
+        var extension = id[1];
+        if (extension == 'html') {
+            $(name).addClass('active');
+        } else {
+            $('#index').addClass('active');
+        }
     }
     window.onload=function(){
         highlight();
